dd0e02d40d63fc610e5c333e559009fa84bc5961
Julio Patiño <franjp132@gmail.com>
2023-02-16 09:53:55 -0500

    Creando componente de estadisticas

--- src/components/Perfil/ValuesProfile.jsx
+++ src/components/Perfil/ValuesProfile.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const ValuesProfile = () => {
   return (
-    <View>
-      <Text>Stadisticas</Text>
+    <View style={styles.valuesProfile}>
+      <Values number='203K' title='Seguidos' />
+      <Values number='1.7M' title='Seguidores' />
+      <Values number='7.1M' title='Likes' />
     </View>
   );
+}
+
+const styles = StyleSheet.create({
+  valuesProfile: {
+    marginTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    width: '70%',
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+  itemValues:{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  numberItem:{
+    fontWeight: '500',
+    fontSize: 22
+  },
+  titleItem:{
+    color: '#999999',
+    fontSize: 15,
+  },
+})
+
+const Values = ({ number, title }) => {
+  return (
+    <View style={styles.itemValues}>
+      <Text style={styles.numberItem}>{number}</Text>
+      <Text style={styles.titleItem}>{title}</Text>
+    </View>
+  )
 }
 
 export default ValuesProfile;
